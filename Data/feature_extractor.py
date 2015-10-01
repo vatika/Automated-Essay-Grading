c28c412120ded07f2bf2f046f8c9d8af2c2048d2
Anurag Ghosh <ghoshanurag1995@gmail.com>
2015-10-02 00:56:00 +0530

    Point Class added

--- Data/feature_extractor.py
+++ Data/feature_extractor.py
@@ -1,6 +1,18 @@
 # Copyright 2015 - Anurag Ghosh, Vatika Harlalka, Abhijeet Kumar
 import csv
 
+class Point:
+    def __init__(self,essay_id,essay_set,essay_str,r1d1,r2d1):
+        this.essay_id = essay_id
+        this.essay_set = essay_set
+        this.essay_str = essay_str
+        this.r1d1 = r1d1
+        this.r2d1 = r2d1
+        this.features = []
+    def __str__():
+        feature_str = ','.join(this.features)
+        return ','.join([this.essay_id,this.essay_set,str(this.r2d1 + this.r1d1),feature_str])
+
 def make_points():
     with open('training_set.csv', 'rb') as f:
         csv_rows = list(csv.reader(f, delimiter = ','))
