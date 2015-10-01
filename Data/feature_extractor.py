3480b33befed0d7cda0ad408d7b54988c2292c67
Anurag Ghosh <ghoshanurag1995@gmail.com>
2015-10-02 03:10:24 +0530

    TODO diction tags

--- Data/feature_extractor.py
+++ Data/feature_extractor.py
@@ -32,11 +32,14 @@ class Point:
 
     def numerical_features(self):
         self.features.append(len(self.essay_str.split('.'))) # Number of sentences
+        for sentence in self.essay_str.split('.'):
+            tmp_tokens = word_tokenize(sentence)
+            nltk.pos_tag(tmp_tokens)
         no_punctuation = self.essay_str.lower().translate(None, string.punctuation)
-        tokens = nltk.word_tokenize(no_punctuation)
-        self.features.append(len(tokens)) # Number of tokens
-        self.features.append(float(len(''.join(tokens)))/float(len(tokens))) # Average size of token
-        self.features.append(len([1 for token in tokens if enchant_dict.check(token) == False])) # Number of misspelled words
+        self.tokens = nltk.word_tokenize(no_punctuation) # Yes, A new self variable. Sorry.
+        self.features.append(len(self.tokens)) # Number of tokens
+        self.features.append((float(len(''.join(self.tokens)))/float(len(self.tokens)))*10) # Average size of token * 10
+        self.features.append(len([1 for token in self.tokens if enchant_dict.check(token) == False])) # Number of misspelled words
 
     def get_all_features(self):
         self.numerical_features()
@@ -57,7 +60,7 @@ def make_points():
             out_file = open('features_' + str(index) + '.csv','w')
             for row in csv_rows:
                 if row[1] == str(3):
-                    p = Point(row[0],row[1],row[2], int(row[6]))
+                    p = Point(row[0], row[1], row[2], int(row[6]))
                     out_file.write(str(p))
 
 def partition_essays():
