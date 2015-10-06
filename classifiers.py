368ff0afc5b28f280c089fb623c74651b31eea66
Anurag Ghosh <ghoshanurag1995@gmail.com>
2015-10-07 00:39:08 +0530

    Refactor

--- classifiers.py
+++ classifiers.py
@@ -8,7 +8,7 @@ import numpy as np
 import csv
 import sklearn
 import nltk
-import sys
+import weighted_kappa as own_wp
 
 class SVR:
     pass
@@ -30,12 +30,12 @@ class Linear_Regression:
         temp = ["%.10f" % x for x in self.theta]
         s =  ' '.join(temp)
         return s
-        
+
     def calculate_cost(self):
         temp = np.matrix(self.Y-self.X.dot(self.theta))
         self.J = temp.T.dot(temp)
-        
-    def gradient_descent(self): 
+
+    def gradient_descent(self):
         for i in range(self.max_limit):
             self.calculate_cost()
             P = self.X.dot(self.theta)
@@ -46,148 +46,61 @@ class Linear_Regression:
             np.seterr(all="raise")
             self.theta = self.theta - update;
 
-            
+
     def predict(self,x):
         return min(3,sum( x*self.theta))
-    
+
     def execute(self,X_test,Y_test):
         self.gradient_descent();
         P = np.zeros(len(Y_test))
         for i in range(len(X_test)):
             P[i] = self.predict(X_test[i])
         P = np.round(P);
-        return quadratic_weighted_kappa(Y_test,P, 0, 3)    
-
-
-def confusion_matrix(rater_a, rater_b, min_rating=None, max_rating=None):
-    """
-    Returns the confusion matrix between rater's ratings
-    """
-#    print rater_a.flatten()
-    assert(len(rater_a) == len(rater_b))
-    if min_rating is None:
-        min_rating = min(rater_a + rater_b)
-    if max_rating is None:
-        max_rating = max(rater_a + rater_b)
-    num_ratings = int(max_rating - min_rating + 1)
-
-    conf_mat = [[0 for i in range(num_ratings)] for j in range(num_ratings)]
-    for a, b in zip(rater_a, rater_b):
-        try:
-            conf_mat[a[0] - min_rating][b - min_rating] += 1
-        except:
-            print "human score = " + str(a[0]) + " machine score = " + str(b)
-    return conf_mat
-
-
-
-def quadratic_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None):
-    """
-    Calculates the quadratic weighted kappa
-    quadratic_weighted_kappa calculates the quadratic weighted kappa
-    value, which is a measure of inter-rater agreement between two raters
-    that provide discrete numeric ratings.  Potential values range from -1
-    (representing complete disagreement) to 1 (representing complete
-    agreement).  A kappa value of 0 is expected if all agreement is due to
-    chance.
-    quadratic_weighted_kappa(rater_a, rater_b), where rater_a and rater_b
-    each correspond to a list of integer ratings.  These lists must have the
-    same length.
-    The ratings should be integers, and it is assumed that they contain
-    the complete range of possible ratings.
-    quadratic_weighted_kappa(X, min_rating, max_rating), where min_rating
-    is the minimum possible rating, and max_rating is the maximum possible
-    rating
-    """
-    k= rater_a.flatten()
-
-    rater_a = np.array(rater_a, dtype=int)
-    rater_b = np.array(rater_b, dtype=int)
-    assert(len(rater_a) == len(rater_b))
-    if min_rating is None:
-        min_rating = min(min(rater_a), min(rater_b))
-    if max_rating is None:
-        max_rating = max(max(rater_a), max(rater_b))
-    conf_mat = confusion_matrix(rater_a, rater_b,
-                                min_rating, max_rating)
-    num_ratings = len(conf_mat)
-    num_scored_items = float(len(rater_a))
-
-    hist_rater_a = histogram(rater_a, min_rating, max_rating)
-    hist_rater_b = histogram(rater_b, min_rating, max_rating)
-
-    numerator = 0.0
-    denominator = 0.0
-
-    for i in range(num_ratings):
-        for j in range(num_ratings):
-            expected_count = (hist_rater_a[i] * hist_rater_b[j]
-                              / num_scored_items)
-            d = pow(i - j, 2.0) / pow(num_ratings - 1, 2.0)
-            numerator += d * conf_mat[i][j] / num_scored_items
-            denominator += d * expected_count / num_scored_items
-
-    return 1.0 - numerator / denominator
-
-def histogram(ratings, min_rating=None, max_rating=None):
-    """
-    Returns the counts of each type of rating that a rater made
-    """
-    if min_rating is None:
-        min_rating = min(ratings)
-    if max_rating is None:
-        max_rating = max(ratings)
-    num_ratings = int(max_rating - min_rating + 1)
-    hist_ratings = [0 for x in range(num_ratings)]
-    for r in ratings:
-        hist_ratings[r - min_rating] += 1
-    return hist_ratings
-
-
+        return own_wp.quadratic_weighted_kappa(Y_test,P, 0, 3)
 
 def data_manipulation():
     for i in range(3,4): #to change after feature extraction done for all sets
-        
+
         # training data
         train_data = []
         with open('./Data/features_'+str(i)+'.csv','r') as in_file:
              csv_content = list(csv.reader(in_file,delimiter=','))
              for row in csv_content:
                 train_data.append(row)
-        
+
         train_data = train_data[1:]   #clip the header
-        train_data = np.matrix(train_data,dtype='float64')  
+        train_data = np.matrix(train_data,dtype='float64')
         Y_train = train_data[:,2].copy()     #actual_values
         X_train = train_data[:,2:].copy()    #actual_data with random bias units
         m = np.size(X_train,axis=0)
         X_train[:,0] = np.ones((m,1)) #bias units modified
-        
+
         #testing data
         test_data = [] # for now both are same modify here to test data
         with open('./Data/features_'+str(i)+'.csv','r') as in_file:
              csv_content = list(csv.reader(in_file,delimiter=','))
              for row in csv_content:
                 test_data.append(row)
-                
+
         test_data = test_data[1:]   #clip the header
         test_data = np.matrix(test_data,dtype='float64')
-        Y_test = test_data[:,2].copy()     #actual_values       
+        Y_test = test_data[:,2].copy()     #actual_values
         X_test = test_data[:,2:].copy()    #actual_data with random bias units
         m = np.size(X_test,axis=0)
         X_test[:,0] = np.ones((m,1)) #bias units modified
-        
+
         #stroing the results for further use maybe(such as single value predictions) .......
         out_file = open('./classifier_weights/essay_set'+str(i)+'.csv','w')
-        
+
         #Linear Regression
         L = Linear_Regression(X_train,Y_train)
         cohen_kappa_result = L.execute(X_test,Y_test)
         print cohen_kappa_result
         #other techniques coming soon
-        
+
         writer = csv.writer(out_file,delimiter=',')
         writer.writerows([str(L).split()])
         out_file.close();
-    
+
 if __name__=='__main__':
     data_manipulation();
